9c1960bcb9193c808305ed78f5d4b57c314b462c
Pranavnarkhede <114733916+Pranavnarkhede@users.noreply.github.com>
2023-03-28 23:21:18 +0530

    Update Employee.java

--- Assignment-4/Employee.java
+++ Assignment-4/Employee.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
-public class EmployeeSalaryCalc {
+public class Sal {
 public static void main(String args[]) {
-Scanner obj—new Scanner(System.in);
+Scanner obj=new Scanner(System.in);
 Programmer p=new Programmer();
 System.out.println("Enter the basic pay of Programmer");
 p.getEmployeeDetails(obj.nextDouble());
@@ -19,53 +19,73 @@ Professor prof=new Professor();
 System.out.println("Enter the basic pay of Professor");
 prof.getEmployeeDetails(obj.nextDouble());
 prof.cal();
+  obj.close();
+
+ }
+}
+
 class Employee {
 String employeeName;
 int employeeID;
 String address;
-String maillD;
+String mailID;
 long mobileNumber;
 double da,hra,pf,sc,ns,gs;
 Scanner obj=new Scanner(System.in);
+
+
 void getEmployeeDetails() {
-System.out.println("Enter the Employee Name:
-employeeName=obj. nextLine();
-                   
+System.out.println("Enter the Employee Name:");
+employeeName=obj.nextLine();
 System.out.println("Enter the Employee Address: ");
 address=obj.nextLine();
 System.out.println("Enter the Employee Mail ID:");
-maillD=obj.nextLine();
+mailID=obj.nextLine();
 System.out.println("Enter the Employee ID:");
-employeeID=obj .nextlnt();
+employeeID=obj.nextInt();
 System.out.println("Enter the Employee Mobile Number:");
 mobileNumber=obj.nextLong();
+}
                    
                    
 void display() {
-System.out.println("Employee Name
-:"+employeeName);
-System.out.println("Employee ID
-:"+employeeID);
-System.out.printIn("EmpIoyee Address
-:"+address);
+System.out.println("Employee Name:"+employeeName);
+System.out.println("Employee ID:"+employeeID);
+System.out.println("EmpIoyee Address:"+address);
 System.out.println("Employee Mail ID :"+mailID);
-System.out.printIn("EmpIoyee Mobile
+System.out.println("EmpIoyee Mobile Number:"+mobileNumber);
+
+}
+
+}
+
+
 class Programmer extends Employee {
 double basicPay;
 public double getBasicPay() {
 return basicPay;
+}
 public void setBasicPay(double basicPay) {
 this. basicPay = basicPay;
+}
 void getEmployeeDetails(double bp) {
   
   super.getEmployeeDetails();
 setBasicPay(bp);
+}
 void cal(){
+da=getBasicPay()* 97/100.0;  
 hra=getBasicPay()* 10/100.0;
-12/100.0;
+pf=getBasicPay()* 12/100.0;
+sc=getBasicPay()* 1/100.0;
+
 gs=getBasicPay()+da+hra+pf+sc;
 ns=gs-pf-sc;
 display();
+}
+
+
+
 void display() {
 super.display();
 System.out.println("Employee Gross Salary:"+gs);
@@ -73,19 +93,24 @@ System.out.println("Employee Net Salary :"+ns);
 }
 }
   
-  class AssistantProfessor extends Employee {
+
+
+
+class AssistantProfessor extends Employee {
 double basicPay;
 public double getBasicPay() {
 return basicPay;
+}
 public void setBasicPay(double basicPay) {
 this.basicPay = basicPay;
 }
   
   
   
-  void getEmployeeDetails(double bp) {
+void getEmployeeDetails(double bp) {
 super.getEmployeeDetails();
 setBasicPay(bp);
+}
 void cal(){
 da=getBasicPay()* 110/ 100.0;
 hra=getBasicPay()*20/100.0;
@@ -94,54 +119,64 @@ sc=getBasicPay()*5/100.0;
 gs=getBasicPay()+da+hra+pf+sc;
 ns=gs-pf-sc;
 display();
+}
 void display() {
 super.display();
-System.out.printIn("EmpIoyee Gross Salary:"+gs)
+System.out.println("EmpIoyee Gross Salary:"+gs);
 System.out. println("Employee Net Salary:"+ns);
 }
 }
     
-  class AssociateProfessor extends Employee {
+ class AssociateProfessor extends Employee {
 double basicPay;
 public double getBasicPay() {
 return basicPay;
+}
 public void setBasicPay(double basicPay) {
 this.basicPay = basicPay;
+}
 void getEmployeeDetails(double bp) {
 super.getEmployeeDetails();
 setBasicPay(bp);
+}
 void cal(){
 da=getBasicPay()* 130/100.0;
 hra=getBasicPay()*30/100.0;
 pf=getBasicPay()* 12/100.0;
-  
-  sc=getBasicPay()* 10/ 100.0;
+sc=getBasicPay()* 10/ 100.0;
 gs=getBasicPay()+da+hra+pf+sc;
 ns=gs-pf-sc;
 display();
+}
 void display(){
 super.display();
 System.out.println("Employee Gross Salary:"+gs);
 System.out.println("Employee Net Salary :"+ns);
+}
+}
   
   
-  class Professor extends Employee{
+ class Professor extends Employee{
 double basicPay;
 public double getBasicPay() {
 return basicPay;
-public void setBasicPay(doubIe basicPay) {
+}
+public void setBasicPay(double basicPay) {
 this.basicPay = basicPay;
+}
 void getEmployeeDetails(double bp) {
 super.getEmployeeDetails();
 setBasicPay(bp);
+}
 void cal() {
-da=getBasicPay()*140/IOO.O;
-hra=getBasicPay()*40/IOO.O;
+da=getBasicPay()*140/100.0;
+hra=getBasicPay()*40/100.0;
 pf=getBasicPay()* 12/100.0;
 sc=getBasicPay()* 15/100.0;
 gs=getBasicPay()+da+hra+pf+sc;
 ns=gs-pf-sc;
 display();
+}
 void display() {
   
   super.display();
@@ -149,3 +184,4 @@ System.out.println("Employee Gross Salary:"+gs);
 System.out.println("Employee Net Salary :"+ns);
   
 }
+ }
